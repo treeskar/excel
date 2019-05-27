61a1859f4f96e1f6d94618cddb97a5a6d9e87342
treeskar <treeskar@gmail.com>
2019-05-27 23:44:31 +0300

    feat(release): add Changelog plugin

--- release.config.js
+++ release.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      { changelogFile: 'CHANGELOG.md' },
+    ],
+    '@semantic-release/git',
     '@semantic-release/github',
   ]
 };
